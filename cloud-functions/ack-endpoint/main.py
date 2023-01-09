e7f0e1e5586bf532558623c984546023b7173853
sotair <krzys.sota@gmail.com>
2023-01-09 20:31:32 +0100

    ack endpoint cloud function src code

--- cloud-functions/ack-endpoint/main.py
+++ cloud-functions/ack-endpoint/main.py
@@ -1,2 +1,16 @@
-def handle_request(*args, **kwargs):
-    print('Triggered')
+import functions_framework
+from google.cloud import firestore
+from google.cloud import logging
+
+
+@functions_framework.http
+def handle_request(request):
+    args = request.args
+    uuid = args["uuid"]
+    admin = args["admin"]
+    db = firestore.Client(project='irio-solution')
+    db.collection(u'emails_sent').document(u"document_to_delete").delete()
+    logging_client = logging.Client()
+    logger = logging_client.logger("outages")
+    logger.log_text(f"Admin {admin} acknowledged outage {uuid}.")
+    return '<html><head>Thank you for acknowledging the outage.</head></html>'
