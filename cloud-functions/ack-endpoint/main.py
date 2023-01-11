c94ec6e43d438e513379ce31bcbbe07afd4ef523
sotair <krzys.sota@gmail.com>
2023-01-11 12:55:14 +0100

    commit to test workflow

--- cloud-functions/ack-endpoint/main.py
+++ cloud-functions/ack-endpoint/main.py
@@ -8,9 +8,12 @@ def handle_request(request):
     args = request.args
     uuid = args["uuid"]
     admin = args["admin"]
+
     db = firestore.Client(project='irio-solution')
     db.collection(u'emails_sent').document(u"document_to_delete").delete()
+
     logging_client = logging.Client()
     logger = logging_client.logger("outages")
     logger.log_text(f"Admin {admin} acknowledged outage {uuid}.")
+    
     return '<html><head>Thank you for acknowledging the outage.</head></html>'
