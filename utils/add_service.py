6bee5c00067bd9e73d4309aa6b4328d296fa14a9
Maciej Pióro <m@ciekpio.ro>
2023-01-24 00:58:54 +0100

    Add utility for adding services

---
+++ utils/add_service.py
@@ -0,0 +1,93 @@
+import argparse
+from hashlib import md5
+import os
+import asyncio
+from google.cloud import firestore, bigquery
+
+# admin_mail1: "example@gmail.com" (string)
+# admin_mail2: "example@gmail.com" (string)
+# alert_window_minutes: 1 (number)
+# allowed_response_time_minutes: 1 (number)
+# check_interval_minutes: 1 (number)
+# first_bad_response_time: null (null)
+# last_ok_response_time: "2023-01-23 23:39:51" (string)
+# last_response_time: "2023-01-23 23:39:51"
+
+
+async def main(
+    service: str,
+    bigquery_table: str,
+    firestore_collection: str,
+    admin1: str,
+    admin2: str,
+    check_interval_minutes: int,
+    alert_window_minutes: int,
+    allowed_response_time_minutes: int,
+):
+    bigquery_client = bigquery.Client()
+    db = firestore.Client()
+    service_digest = md5(service.encode("utf-8")).hexdigest()
+    db.collection(firestore_collection).document(service_digest).set(
+        {
+            "admin_mail1": admin1,
+            "admin_mail2": admin2,
+            "check_interval_minutes": check_interval_minutes,
+            "alert_window_minutes": alert_window_minutes,
+            "allowed_response_time_minutes": allowed_response_time_minutes,
+            "first_bad_response_time": None,
+            "last_ok_response_time": None,
+            "last_response_time": None,
+        }
+    )
+    bigquery_client.query(
+        f"INSERT INTO `{bigquery_table}` (url, updated_at) VALUES ('{service}', CURRENT_DATE())"
+    ).result()
+    print("Done!")
+
+
+if __name__ == "__main__":
+    if not os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
+        print("GOOGLE_APPLICATION_CREDENTIALS not set. Exiting...")
+        exit(1)
+    parser = argparse.ArgumentParser(
+        description="Add a service to the k8s cluster",
+    )
+    parser.add_argument("--service", type=str, help="The service to add", required=True)
+    parser.add_argument("--bigquery", type=str, help="BigQuery Table", required=True)
+    parser.add_argument(
+        "--firestore", type=str, help="Firestore Collection", required=True
+    )
+    parser.add_argument("--admin1", type=str, help="Admin 1", required=True)
+    parser.add_argument("--admin2", type=str, help="Admin 2", required=True)
+    parser.add_argument(
+        "--check_interval_minutes",
+        type=int,
+        help="Check interval in minutes",
+        required=True,
+    )
+    parser.add_argument(
+        "--alert_window_minutes",
+        type=int,
+        help="Alert window in minutes",
+        required=True,
+    )
+    parser.add_argument(
+        "--allowed_response_time_minutes",
+        type=int,
+        help="Allowed response time in minutes",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    asyncio.run(
+        main(
+            service=args.service,
+            bigquery_table=args.bigquery,
+            firestore_collection=args.firestore,
+            admin1=args.admin1,
+            admin2=args.admin2,
+            check_interval_minutes=args.check_interval_minutes,
+            alert_window_minutes=args.alert_window_minutes,
+            allowed_response_time_minutes=args.allowed_response_time_minutes,
+        )
+    )
