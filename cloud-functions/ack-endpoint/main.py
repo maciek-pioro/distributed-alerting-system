005f57e798648355e2e3a1385ddd59ea56ef2270
Maciej Pióro <m@ciekpio.ro>
2023-01-29 00:35:10 +0100

    Put envs in secrets

--- cloud-functions/ack-endpoint/main.py
+++ cloud-functions/ack-endpoint/main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import json
 
 PROJECT_ID = os.getenv("GCP_PROJECT")
-EMAILS_SENT_COLLECTION_NAME = os.getenv("EMAILS_SENT_COLLECTION")
+EMAILS_SENT_COLLECTION_NAME = os.getenv("EMAIL_COLLECTION")
 
 
 @functions_framework.http
@@ -17,10 +17,18 @@ def handle_request(request):
 
     db = firestore.Client(project=PROJECT_ID)
     doc_ref = db.collection(EMAILS_SENT_COLLECTION_NAME).document(uuid)
-    doc_ref.set({u'ack_by': admin, 'ack': True}, merge=True)
+    doc_ref.set({"ack_by": admin, "ack": True}, merge=True)
 
     logging_client = logging.Client()
     logger = logging_client.logger("outages")
-    logger.log_text(json.dumps({"service": doc_ref.get().to_dict()['url'], "outage": uuid, "event": f"{admin} admin ack {datetime.now()}"}))
+    logger.log_text(
+        json.dumps(
+            {
+                "service": doc_ref.get().to_dict()["url"],
+                "outage": uuid,
+                "event": f"{admin} admin ack {datetime.now()}",
+            }
+        )
+    )
 
-    return '<html><head>Thank you for acknowledging the outage.</head></html>'
+    return "<html><head>Thank you for acknowledging the outage.</head></html>"
